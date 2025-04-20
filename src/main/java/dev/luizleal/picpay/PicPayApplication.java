6847406f275aed7647f155308a25dd4fb7e368b0
luizleal <luizandreleal100@gmail.com>
2025-04-20 09:56:22 -0300

    [feat]: Add initial files

---
+++ src/main/java/dev/luizleal/picpay/PicPayApplication.java
@@ -0,0 +1,13 @@
+package dev.luizleal.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luizleal/picpay/PicPayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luizleal.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
